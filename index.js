6077c652e964a5ab89c5661af198edaea0b08f0d
JUNIORPRUEVA <jrdigitalrd@gmail.com>
2025-12-03 12:20:49 -0400

    Update index.js cabio 1

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000
 ;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Servidor FULLPOS API corriendo en http://10.0.2.15:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor FULLPOS API corriendo en puerto ${PORT}`);
 });
